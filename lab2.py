200177f7eaa903b49d5804116cdb5909a6a8b0a7
Jessie Chen <jessch.2140@gmail.com>
2021-06-23 15:17:49 -0700

    added variable practice, creating them and type()

---
+++ lab2.py
@@ -0,0 +1,24 @@
+print()
+print('Hello World!!!!!')
+print()
+
+# STEP 1
+name="Jisung" # string
+age=19 # integer
+salary=37.44 # float
+plays_basketball=False # boolean
+jersey_number="5" # string
+
+# STEP 2
+car_name="Chevy" # string
+x=50 
+my_first_name="Jessie"
+
+print(type(name))
+print(type(age))
+print(type(salary))
+print(type(plays_basketball))
+print(type(jersey_number))
+print(type(car_name))
+print(type(x))
+print(type(my_first_name))
