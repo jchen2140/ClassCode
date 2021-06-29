94f629f915a5e5c02418101353fb669922543fa9
Jessie Chen <jessch.2140@gmail.com>
2021-06-28 17:10:17 -0700

    lab 4 work

--- lab4.py
+++ lab4.py
@@ -11,6 +11,12 @@ def squareArea(side):
     return area
 print(squareArea(4))
 
+def rect_area(length, width):
+    area = length*width
+    return area
+print("The area of a 4 x 6 rectangle is:")
+print(rect_area(4,6))
+
 
 # 2nd half of lab 4
 # STEP 2
@@ -45,30 +51,55 @@ def foodDifference(Dict, item1, item2):
     print("The difference between" + item1 + " and " + item2 + " is $" + str(foodDifference))
 foodDifference(price_difference, "Fries", "Hamburger")
 
+#OR
+
+def price_difference(item1, item2):
+    price1 = Menu[item1]
+    price2 = Menu[item2]
+    difference = price1 - price2
+    if price1 > price2:
+        difference = price1 - price2
+    else:
+        difference = price2 - price1
+    difference = price1 - price2
+    return difference
+    
+print("The difference between Hamburger and Fries: $" + str(price_difference("Fries", "Hamburger")))
+
 # Step 4
 
 restock = {"Yeezy": 8, "Air Force": 5}
 
-def shoe_number(Dict, shoe1, num):
-    shoe_number = Dict[shoe1] *num
+def shoe_number(shoe1, num):
+    shoe_number = restock[shoe1] *num
     print(shoe_number)
-shoe_number(restock, "Yeezy", 3)
+shoe_number("Yeezy", 3)
 
+shoes = {'Jordan 13': 1, 'Yeezy': 8, 'Foamposite': 10, 'Air Max': 5, 'SB Dunk': 20 }
+def restock(shoe_type, count):
+    shoe_type = shoes[shoe_type] * count
+    return shoes[shoe_type]
+    # 8 = 8*3
+    # 24
+    #shoes[Yeezy] =
+
+restock("Yeezy", 3)
+print(restock("Yeezy", 3))
 # Step 5
 clearance_sale = {"SB Dunk": 20, "Air Force": 40}
 
-def product_price(Dict, item1, num):
-    product_price = Dict[item1] /num
+def product_price(item1, num):
+    product_price = clearance_sale[item1] /num
     print(product_price)
-product_price(clearance_sale, "SB Dunk", 5)
+product_price("SB Dunk", 5)
 
 # Step 6
 number_of_shoes = {"SB Dunk": 20, "Air Force": 40}
 
-def bigger_amount(Dict, shoe1, shoe2):
-    bigger_amount = Dict[shoe1]//Dict[shoe2]
-    print(bigger_amount)
-bigger_amount(number_of_shoes, "Air Force", "SB Dunk")
+def shoe_ratio(shoe1, shoe2):
+    shoe_ratio = number_of_shoes[shoe1]//number_of_shoes[shoe2]
+    print(shoe_ratio)
+shoe_ratio("Air Force", "SB Dunk")
 
 
 
